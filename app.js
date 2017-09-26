750e705c97c148c82812618ac98c892a4ed5fc2e
Joe Seiden <joe.seiden214@gmail.com>
2017-09-26 15:06:11 -0700

    Create chat server

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const app = express();
 const http = require('http').Server(app);
 const path = require('path');
 
+const chatServer = require('./lib/chatServer');
+chatServer.listen(http);
+
+const PORT = 8000;
+
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "public", "/index.html"));
 });
 
-http.listen(8000, () => {
-  console.log('Listening on port 8000 ');
+http.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
